96f894baca60f2f7870bef6d5bca67d444e70528
Angel Aguero <angelagueror23@gmail.com>
2023-03-03 09:29:19 -0400

    Inicion de testQ

---
+++ src/main/java/org/angel/testQ/TestQApplication.java
@@ -0,0 +1,13 @@
+package org.angel.testQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestQApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestQApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/angel/testQ/TestQApplicationTests.java
@@ -0,0 +1,13 @@
+package org.angel.testQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestQApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
